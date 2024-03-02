99e75d33b564be011b421ff7cfd2f2b5ca63f135
cubeiceman <elliott.w.suh@gmail.com>
2024-03-02 13:02:47 -0800

    button changes status based on hovering and clicking

--- Buttons.py
+++ Buttons.py
@@ -1,11 +1,27 @@
-class Button:
-    def __init__(self, x, y, w, h, color=(50, 205, 50)):
-        self.x = x
-        self.y = y
-        self.width = w
-        self.height = h
-        self.color = color
-        self.clicked = False
+import pygame
 
-    def foo(self):
-        pass
+
+class Button:
+    def __init__(self, x, y, w, h, base_color, hover_color, pressed_color):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.colors_list = [base_color, hover_color, pressed_color]
+        self.color_pick = 0  # pick a color based on what's happening
+        # 0: mouse is not touching color box
+        # 1: mouse is hovering over color box
+        # 2: mouse is holding down over color box
+
+    def change_color(self, button_status):
+        if button_status == "off":
+            self.color_pick = 0
+        elif button_status == "hover":
+            self.color_pick = 1
+        elif button_status == "holding":
+            self.color_pick = 2
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.colors_list[self.color_pick], self.rect)
+
+
+class Generate_Button(Button):
+    def __init__(self, x, y, w, h):
+        super().__init__(x, y, w, h, (34, 139, 34), (0, 128, 0), (0, 100, 0))
