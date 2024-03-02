9717e050adc166fe4332783dc3f93d91aa42bb7a
cubeiceman <elliott.w.suh@gmail.com>
2024-03-02 12:06:47 -0800

    added button class

---
+++ Buttons.py
@@ -0,0 +1,11 @@
+class Button:
+    def __init__(self, x, y, w, h, color=(50, 205, 50)):
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+        self.color = color
+        self.clicked = False
+
+    def foo(self):
+        pass
